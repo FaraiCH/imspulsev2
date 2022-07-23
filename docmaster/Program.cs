a9bfded84bfac07b465f97b8d7fb94fdcde136dc
FARAI Chakarisa <faraichaka@gmail.com>
2022-07-23 17:16:10 +0200

    update doc master

---
+++ docmaster/Program.cs
@@ -0,0 +1,53 @@
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using docmaster.Data;
+using docmaster.Areas.Identity.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+var connetionString = builder.Configuration.GetConnectionString("Default");
+
+builder.Services.AddDbContext<AuthDbContext>(options =>
+    options.UseMySql(connetionString, ServerVersion.AutoDetect(connetionString)));
+
+builder.Services.AddDefaultIdentity<docmasterUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<AuthDbContext>();
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddRazorPages();
+//DB Context
+//builder.Services.AddDbContext<AppDbContext>(options => {
+//    var connetionString = builder.Configuration.GetConnectionString("Default");
+//    options.UseMySql(connetionString, ServerVersion.AutoDetect(connetionString));
+//});
+
+//builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+//    .AddEntityFrameworkStores<AppDbContext>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();;
+
+app.UseAuthorization();
+app.UseAuthentication();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapRazorPages();
+app.Run();
