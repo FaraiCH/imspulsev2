719c54a8062019f40d765a7dc87f69762943b6c4
FARAI Chakarisa <faraichaka@gmail.com>
2022-07-25 21:03:52 +0200

    Update

--- docmaster/Controllers/HomeController.cs
+++ docmaster/Controllers/HomeController.cs
@@ -16,25 +16,25 @@ namespace docmaster.Controllers
 
         public IActionResult Index()
         {
-            Document doc = new Document();
-            DocumentBuilder builder = new DocumentBuilder(doc);
+            //Document doc = new Document();
+            //DocumentBuilder builder = new DocumentBuilder(doc);
 
-            // Specify font formatting
-            Font font = builder.Font;
-            font.Size = 16;
-            font.Bold = true;
-            font.Color = System.Drawing.Color.Blue;
-            font.Name = "Arial";
-            font.Underline = Underline.Dash;
+            //// Specify font formatting
+            //Font font = builder.Font;
+            //font.Size = 16;
+            //font.Bold = true;
+            //font.Color = System.Drawing.Color.Blue;
+            //font.Name = "Arial";
+            //font.Underline = Underline.Dash;
 
-            // Specify paragraph formatting
-            ParagraphFormat paragraphFormat = builder.ParagraphFormat;
-            paragraphFormat.FirstLineIndent = 8;
-            paragraphFormat.Alignment = ParagraphAlignment.Justify;
-            paragraphFormat.KeepTogether = true;
+            //// Specify paragraph formatting
+            //ParagraphFormat paragraphFormat = builder.ParagraphFormat;
+            //paragraphFormat.FirstLineIndent = 8;
+            //paragraphFormat.Alignment = ParagraphAlignment.Justify;
+            //paragraphFormat.KeepTogether = true;
 
-            builder.Writeln("A whole paragraph.");
-            doc.Save(@"/var/www/html/imspulse/myone.doc");
+            //builder.Writeln("A whole paragraph.");
+            //doc.Save(@"/var/www/html/imspulse/myone.doc");
             return View();
         }
 
