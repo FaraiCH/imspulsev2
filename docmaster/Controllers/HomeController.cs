50217f11a0484f44b2a5ad9ff539e7272cfeb4d6
Farai Chakarisa <faraichaka@gmail.com>
2022-07-29 13:25:49 +0200

    Remove Aspose

--- docmaster/Controllers/HomeController.cs
+++ docmaster/Controllers/HomeController.cs
@@ -1,4 +1,4 @@
-﻿using Aspose.Words;
+﻿
 using docmaster.Models;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.AspNetCore.Mvc;
@@ -26,25 +26,7 @@ namespace docmaster.Controllers
 
         public IActionResult Index()
         {
-            Document doc = new Document();
-            DocumentBuilder builder = new DocumentBuilder(doc);
-
-            // Specify font formatting
-            Font font = builder.Font;
-            font.Size = 16;
-            font.Bold = true;
-            font.Color = System.Drawing.Color.Blue;
-            font.Name = "Arial";
-            font.Underline = Underline.Dash;
-
-            // Specify paragraph formatting
-            ParagraphFormat paragraphFormat = builder.ParagraphFormat;
-            paragraphFormat.FirstLineIndent = 8;
-            paragraphFormat.Alignment = ParagraphAlignment.Justify;
-            paragraphFormat.KeepTogether = true;
-
-            builder.Writeln("A whole paragraph.");
-            doc.Save(@"/var/www/html/imspulse/danny.doc");
+      
             return View();
         }
 
