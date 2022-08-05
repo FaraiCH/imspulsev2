daa966bebcc1d682867c314337cb5cb475f86bf3
Farai Chakarisa <faraichaka@gmail.com>
2022-08-05 13:44:08 +0200

    update

--- docmaster/Controllers/HomeController.cs
+++ docmaster/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 ﻿
+using docmaster.Areas.Identity.Data;
 using docmaster.Models;
 using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using Syncfusion.DocIO;
@@ -13,17 +15,18 @@ namespace docmaster.Controllers
 {
     public class HomeController : Controller
     {
-       
 
+        UserManager<docmasterUser> UserManager;
         public PhysicalFileProvider operation;
-        public string basePath = "/var/www/html/imspulse/bunch-box";
+       
         string root = @"wwwroot";
 
         public HomeController(Microsoft.AspNetCore.Hosting.IWebHostEnvironment hostingEnvironment)
         {
-           
+
+            string basePath = "/var/www/html/imspulse/bunch-box/" + UserManager.GetUserAsync(User).Result.Company;
             this.operation = new PhysicalFileProvider();
-            this.operation.RootFolder(this.basePath);
+            this.operation.RootFolder(basePath);
         }
 
         public IActionResult Index()
@@ -265,7 +268,8 @@ namespace docmaster.Controllers
         }
         public object FileOperations([FromBody] FileManagerDirectoryContent args)
         {
-            var fullPath = this.basePath.Replace('\\', '/') + args.Path;
+            string basePath = "/var/www/html/imspulse/bunch-box/" + UserManager.GetUserAsync(User).Result.Company;
+            var fullPath = basePath.Replace('\\', '/') + args.Path;
             if (args.Action == "delete" || args.Action == "rename")
             {
                 if ((args.TargetPath == null) && (args.Path == ""))
