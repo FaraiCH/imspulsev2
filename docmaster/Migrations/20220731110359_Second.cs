81daad4f8aef9301566427c958aa90048106d30f
FARAI Chakarisa <faraichaka@gmail.com>
2022-08-05 17:19:27 +0200

    update

---
+++ docmaster/Migrations/20220731110359_Second.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace docmaster.Migrations
+{
+    public partial class Second : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Company",
+                table: "AspNetUsers",
+                type: "nvarchar(100)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Company",
+                table: "AspNetUsers");
+        }
+    }
+}
