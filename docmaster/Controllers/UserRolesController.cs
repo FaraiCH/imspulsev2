e33da8c2e1ead3525a30f159b3c110881be33892
Farai Chakarisa <faraichaka@gmail.com>
2022-08-18 10:08:20 +0200

    Update

---
+++ docmaster/Controllers/UserRolesController.cs
@@ -0,0 +1,39 @@
+﻿using docmaster.Areas.Identity.Data;
+using docmaster.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace docmaster.Controllers
+{
+    public class UserRolesController : Controller
+    {
+        private readonly UserManager<docmasterUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        public UserRolesController(UserManager<docmasterUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userManager.Users.ToListAsync();
+            var userRolesViewModel = new List<UserRolesViewModel>();
+            foreach (docmasterUser user in users)
+            {
+                var thisViewModel = new UserRolesViewModel();
+                thisViewModel.UserId = user.Id;
+                thisViewModel.Email = user.Email;
+                thisViewModel.FirstName = user.FirstName;
+                thisViewModel.LastName = user.LastName;
+                thisViewModel.Roles = await GetUserRoles(user);
+                userRolesViewModel.Add(thisViewModel);
+            }
+            return View(userRolesViewModel);
+        }
+        private async Task<List<string>> GetUserRoles(docmasterUser user)
+        {
+            return new List<string>(await _userManager.GetRolesAsync(user));
+        }
+    }
+}
