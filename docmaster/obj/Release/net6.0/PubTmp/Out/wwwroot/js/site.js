3b889de4ada96b8e3ae29aac37215ebeec91cfd7
FARAI Chakarisa <faraichaka@gmail.com>
2022-09-25 18:22:13 +0200

    Update

--- docmaster/obj/Release/net6.0/PubTmp/Out/wwwroot/js/site.js
+++ docmaster/obj/Release/net6.0/PubTmp/Out/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function ()
+{ /* code here */
+    $(".unpro").hide();
+    $(".toast_container").hide();
+});
