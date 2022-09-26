5696e41d8d209d07c45e97f6cf98ec1f13d08276
Farai Chakarisa <faraichaka@gmail.com>
2022-09-26 11:49:20 +0200

    update

---
+++ docmaster/Controllers/DocumentListController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace docmaster.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DocumentListController : ControllerBase
+    {
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+    }
+}
