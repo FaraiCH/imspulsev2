cc9f72909a709ed24662a4b5e3b2201844527a45
Farai Chakarisa <faraichaka@gmail.com>
2022-09-26 13:21:23 +0200

    update

--- docmaster/Controllers/DocumentListController.cs
+++ docmaster/Controllers/DocumentListController.cs
@@ -8,10 +8,15 @@ namespace docmaster.Controllers
     public class DocumentListController : ControllerBase
     {
         // GET api/values/5
-        [HttpGet("{id}")]
-        public ActionResult<string> Get(int id)
+        [HttpGet("Company={company}")]
+        public ActionResult<string> Get(string company)
         {
             return "value";
+        }
+        [HttpGet("Company={company}&WichType={passdoc}")]
+        public ActionResult<string> Passowrddoc(string company, string passdoc)
+        {
+            return "document password";
         }
     }
 }
