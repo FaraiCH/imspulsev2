ae2a5b0e4bdb2a178ca1d05ff67a6fbe2dd09c44
Farai Chakarisa <faraichaka@gmail.com>
2022-09-29 09:24:47 +0200

    update

--- docmaster/Controllers/DocumentListController.cs
+++ docmaster/Controllers/DocumentListController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Aspose.Words;
+using docmaster.Areas.Identity.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 
@@ -8,15 +11,28 @@ namespace docmaster.Controllers
     [ApiController]
     public class DocumentListController : ControllerBase
     {
+        UserManager<docmasterUser> _userManager;
         // GET api/values/5
         [HttpGet("Company={company}&WichType={passdoc}&Mode={mode}")]
-        public ActionResult<string> GetDocument(string company, string passdoc, string mode)
+        public async Task<ActionResult<string>> GetDocument(string company, string passdoc, string mode)
         {
             var documents = new List<Tuple<string, string, string>>();
-            documents.Add(new Tuple<string, string, string>("12", "something", "something"));
-            string json = JsonConvert.SerializeObject(new {            
-                documents           
+            foreach (string d in Directory.GetDirectories("/var/www/html/imspulse/bunch-box/" + company))
+            {
+             
+                foreach (string f in Directory.GetFiles(d))
+                {
+                    FileFormatInfo info = FileFormatUtil.DetectFileFormat(d + "Document.doc");
+                    documents.Add(new Tuple<string, string, string>(d, "something", "something"));
+                }
+                  
+             
+            }
+            string json = JsonConvert.SerializeObject(new
+            {
+                documents
             });
+
             return json;
         }
        
