1e8cf841daf2b5b9fcb8ad7aa4379f648683b62b
FARAI Chakarisa <faraichaka@gmail.com>
2022-10-26 19:15:10 +0200

    Update

--- docmaster/Controllers/HomeController.cs
+++ docmaster/Controllers/HomeController.cs
@@ -299,7 +299,7 @@ namespace docmaster.Controllers
             catch (Exception ex)
             {
 
-                return new JsonResult("Password protected " + ex.Message);
+                return new JsonResult("Error " + ex.Message);
             }
           
         }
