95f35e50721ab5def43e73ee6452f26006879aa6
FARAI Chakarisa <faraichaka@gmail.com>
2022-11-30 14:08:58 +0200

    update

--- docmaster/Controllers/DocumentListController.cs
+++ docmaster/Controllers/DocumentListController.cs
@@ -44,41 +44,6 @@ namespace docmaster.Controllers
             return json;
         }
 
-        [HttpGet("DocumnetAll={company}")]
-        public async Task<ActionResult<string>> GetAll(string company, string passdoc, string mode)
-        {
-            var documents = new List<Tuple<string, int, int, int>>();
-            int operations = 0;
-            int resources = 0;
-            int qms = 0;
-            DirectoryInfo op = new DirectoryInfo("/var/www/html/imspulse/bunch-box/" + company + "/Operations");
-            DirectoryInfo re = new DirectoryInfo("/var/www/html/imspulse/bunch-box/" + company + "/Resources");
-            DirectoryInfo q = new DirectoryInfo("/var/www/html/imspulse/bunch-box/" + company + "/QMS");
-            FileInfo[] directoryre = re.GetFiles("*", System.IO.SearchOption.AllDirectories);
-            FileInfo[] directoryop = re.GetFiles("*", System.IO.SearchOption.AllDirectories);
-            FileInfo[] directoryqms = re.GetFiles("*", System.IO.SearchOption.AllDirectories);
-            //string[] directory = Directory.GetFiles(@"C:\Testing\", "*", System.IO.SearchOption.AllDirectories);       
-            foreach (FileInfo f in directoryre)
-            {
-                resources++;
-            }
-            foreach (FileInfo f in directoryqms)
-            {
-                qms++;
-            }
-            foreach (FileInfo f in directoryop)
-            {
-                operations++;
-            }
-
-            documents.Add(new Tuple<string, int, int, int>("RE QMS OP",resources, qms, operations));
-            string json = JsonConvert.SerializeObject(new
-            {
-                documents
-            });
-
-            return json;
-        }
 
 
     }
---
+++ docmaster/Controllers/QMSController.cs
@@ -0,0 +1,54 @@
+﻿using docmaster.Areas.Identity.Data;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace docmaster.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("MyPolicy")]
+    public class QMSController : ControllerBase
+    {
+
+        UserManager<docmasterUser> _userManager;
+
+        [HttpGet("DocumnetAll={company}")]
+        public async Task<ActionResult<string>> GetAll(string company, string passdoc, string mode)
+        {
+            var documents = new List<Tuple<string, int, int, int>>();
+            int operations = 0;
+            int resources = 0;
+            int qms = 0;
+            DirectoryInfo op = new DirectoryInfo("/var/www/html/imspulse/bunch-box/" + company + "/Operations");
+            DirectoryInfo re = new DirectoryInfo("/var/www/html/imspulse/bunch-box/" + company + "/Resources");
+            DirectoryInfo q = new DirectoryInfo("/var/www/html/imspulse/bunch-box/" + company + "/QMS");
+            FileInfo[] directoryre = re.GetFiles("*", System.IO.SearchOption.AllDirectories);
+            FileInfo[] directoryop = re.GetFiles("*", System.IO.SearchOption.AllDirectories);
+            FileInfo[] directoryqms = re.GetFiles("*", System.IO.SearchOption.AllDirectories);
+            //string[] directory = Directory.GetFiles(@"C:\Testing\", "*", System.IO.SearchOption.AllDirectories);       
+            foreach (FileInfo f in directoryre)
+            {
+                resources++;
+            }
+            foreach (FileInfo f in directoryqms)
+            {
+                qms++;
+            }
+            foreach (FileInfo f in directoryop)
+            {
+                operations++;
+            }
+
+            documents.Add(new Tuple<string, int, int, int>("RE QMS OP", resources, qms, operations));
+            string json = JsonConvert.SerializeObject(new
+            {
+                documents
+            });
+
+            return json;
+        }
+    }
+}
