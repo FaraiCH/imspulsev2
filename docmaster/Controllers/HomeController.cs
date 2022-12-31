c7e7a6aa5d09616e9c712253f9113f9a36a0a717
FARAI Chakarisa <faraichaka@gmail.com>
2022-12-31 15:44:21 +0200

    update

--- docmaster/Controllers/HomeController.cs
+++ docmaster/Controllers/HomeController.cs
@@ -671,34 +671,6 @@ namespace docmaster.Controllers
             return new JsonResult("Revision Successful");
         }
 
-
-        public async Task<IActionResult> Hide([FromBody] ProtectModel payload)
-        {
-            string[] folders = new string[] { };
-            var user = await _userManager.GetUserAsync(this.User);
-            int counter = 0;
-            using (var conn = new MySqlConnection("Server=92.205.25.31; Database=imspulse; Uid=manny; Pwd=@Paradice1;"))
-            {
-                conn.Open();
-
-                //// Retrieve all rows
-                using (var cmd = new MySqlCommand("SELECT * FROM imspulse.farai_document_hiddens", conn))
-                {
-                    using (var reader = await cmd.ExecuteReaderAsync())
-                    {
-                        while (await reader.ReadAsync())
-                        {
-                            folders[counter] = reader.GetString(1);  
-                          
-                            counter++;
-                        }
-                    }
-                }
-            }
-            
-            return new JsonResult(folders);
-        }
-
         public async Task<IActionResult> Hide2([FromBody] ProtectModel payload)
         {
             var user = await _userManager.GetUserAsync(this.User);
