6f40465f19ca70ad3321d9413dc664bf459c598e
FARAI Chakarisa <faraichaka@gmail.com>
2023-01-13 23:04:55 +0200

    update

--- docmaster/Controllers/HomeController.cs
+++ docmaster/Controllers/HomeController.cs
@@ -112,112 +112,31 @@ namespace docmaster.Controllers
             Exec("sudo chmod 775 -R /var/www/html/imspulse/bunch-box");
 
             FileManagerResponse uploadResponse;
-            var user = await _userManager.GetUserAsync(this.User);
-            double fCount = GetDirectorySize(this.basePath + "/" + user.Company);
-            foreach (var item in uploadFiles)
+            foreach (var file in uploadFiles)
             {
-          
-
-                if (item.FileName.Contains(".doc") || item.FileName.Contains(".xls") || item.FileName.Contains(".xls") || item.FileName.Contains(".csv") || item.FileName.Contains(".ppt") || item.FileName.Contains(".pdf") || item.FileName.Contains(".vsd") || item.FileName.Contains(".pub") || item.FileName.Contains(".txt"))
+                var folders = (file.FileName).Split('/');
+                // checking the folder upload
+                if (folders.Length > 1)
                 {
-                    var folders = (item.FileName).Split('/');
-                    // checking the folder upload
-                    if (folders.Length > 1)
+                    for (var i = 0; i < folders.Length - 1; i++)
                     {
-                        for (var i = 0; i < folders.Length - 1; i++)
+                        string newDirectoryPath = Path.Combine(this.basePath + path, folders[i]);
+                        if (!Directory.Exists(newDirectoryPath))
                         {
-                            string newDirectoryPath = Path.Combine(this.basePath + path, folders[i]);
-                            if (!Directory.Exists(newDirectoryPath))
-                            {
-                                this.operation.ToCamelCase(this.operation.Create(path, folders[i]));
-                            }
-                            path += folders[i] + "/";
+                            this.operation.ToCamelCase(this.operation.Create(path, folders[i]));
                         }
+                        path += folders[i] + "/";
                     }
-
                 }
-                else
-                {
-                    Response.Clear();
-                    Response.ContentType = "application/json; charset=utf-8";
-                    Response.StatusCode = Convert.ToInt32("1111");
-                    Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = "The Document Manager does not support this file format.";
-                }
-
             }
-
-            if (!this.User.IsInRole("Master"))
+            uploadResponse = operation.Upload(path, uploadFiles, action, null);
+            if (uploadResponse.Error != null)
             {
-                if (this.User.IsInRole("Basic"))
-                {
-
-                    if (fCount > 50)
-                    {
-                        Response.Clear();
-                        Response.ContentType = "application/json; charset=utf-8";
-                        Response.StatusCode = Convert.ToInt32("1111");
-                        Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = "You have reached the maximum uploads allowed on Basic Subscription. Upgrade plan";
-                    }
-                    else
-                    {
-
-                        uploadResponse = operation.Upload(path, uploadFiles, action, null);
-                    }
-
-                }
-                else if (this.User.IsInRole("Premium"))
-                {
-                    if (fCount > 350)
-                    {
-                        Response.Clear();
-                        Response.ContentType = "application/json; charset=utf-8";
-                        Response.StatusCode = Convert.ToInt32("1111");
-                        Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = "You have reached the maximum uploads allowed on Premium Subscription. Upgrade plan";
-                    }
-                    else
-                    {
-
-                        uploadResponse = operation.Upload(path, uploadFiles, action, null);
-                    }
-
-                }
-                else if (this.User.IsInRole("Ultimate"))
-                {
-                    if (fCount > 1000)
-                    {
-                        Response.Clear();
-                        Response.ContentType = "application/json; charset=utf-8";
-                        Response.StatusCode = Convert.ToInt32("1111");
-                        Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = "You have reached your limit";
-                    }
-                    else
-                    {
-
-                        uploadResponse = operation.Upload(path, uploadFiles, action, null);
-                    }
-                }
-                else
-                {
-
-                    Response.Clear();
-                    Response.ContentType = "application/json; charset=utf-8";
-                    Response.StatusCode = Convert.ToInt32("1111");
-                    Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = "You do not have permissions to upload anything.";
-
-                }
+                Response.Clear();
+                Response.ContentType = "application/json; charset=utf-8";
+                Response.StatusCode = Convert.ToInt32(uploadResponse.Error.Code);
+                Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = uploadResponse.Error.Message;
             }
-            else
-            {
-                uploadResponse = operation.Upload(path, uploadFiles, action, null);
-                if (uploadResponse.Error != null)
-                {
-                    Response.Clear();
-                    Response.ContentType = "application/json; charset=utf-8";
-                    Response.StatusCode = Convert.ToInt32(uploadResponse.Error.Code);
-                    Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = uploadResponse.Error.Message;
-                }
-            }
-
             return Content("");
         }
 
